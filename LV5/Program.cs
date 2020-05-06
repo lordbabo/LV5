3c323c30f05385cbfbd60e4268a8df47fd1de8b2
lordbabo <slaven55555@gmail.com>
2020-05-06 19:39:20 +0200

    Add project files.

---
+++ LV5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LV5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IShipable> ProductList = new List<IShipable>();
+            Box box = new Box("Box 1");
+            ProductList.Add(new Product("Product 1", 10, 5));
+            ProductList.Add(new Product("Product 2", 20, 8));
+            box.Add(new Box("Box num1"));
+            foreach (IShipable item in ProductList)
+            {
+                box.Add(item);
+            }
+
+
+            ShippingService service = new ShippingService(1.7);
+            double totalPrice = box.Price + service.ShippingPrice(box);
+            Console.WriteLine("Total weight of package your package is " + box.Weight);
+            Console.WriteLine("Total price of your package is" + totalPrice);
+        }
+    }
+}
---
+++ LV5/ShippingService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LV5
+{
+    class ShippingService  // ZADATAK 2
+    {
+        private double priceOfMassUnit;
+        public ShippingService(double priceOfMassUnit)
+        {
+            this.priceOfMassUnit = priceOfMassUnit;
+        }
+        public double ShippingPrice(Box item)
+        {
+            double shippingPrice = 0;
+            shippingPrice = item.Weight * priceOfMassUnit;
+            return shippingPrice;
+        }
+    }
+}
